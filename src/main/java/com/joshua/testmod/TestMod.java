c170174d353f9f17b9a89123653f9b3972fbadd8
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2015-09-02 21:55:38 +1000

    Added a config

--- src/main/java/com/joshua/testmod/TestMod.java
+++ src/main/java/com/joshua/testmod/TestMod.java
@@ -1,20 +1,27 @@
 package com.joshua.testmod;
 
+import com.joshua.testmod.configuration.ConfigurationHandler;
+import com.joshua.testmod.proxy.IProxy;
+import com.joshua.testmod.reference.Reference;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid="TestMod", name="Test Mod", version="1.7.2-0.1.0.1")
+@Mod(modid= Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION)
 public class TestMod
 {
-    @Mod.Instance("TestMod")
+    @Mod.Instance(Reference.MOD_ID)
     public static TestMod instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
     }
 
     @Mod.EventHandler
