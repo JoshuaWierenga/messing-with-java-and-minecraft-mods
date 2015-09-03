afd4b6b0dfd3a51cf7204ded9ab89c80e70351ba
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2015-09-03 15:25:00 +1000

    Updated version info

--- src/main/java/com/joshua/testmod/reference/Reference.java
+++ src/main/java/com/joshua/testmod/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "TestMod";
     public static final String MOD_NAME = "Test Mod";
-    public static final String VERSION = "1.7.2-0.1.0.1";
+    public static final String VERSION = "1.7.2-0.1.0.4";
     public static final String CLIENT_PROXY_CLASS = "com.joshua.testmod.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.joshua.testmod.proxy.ServerProxy";
 }
